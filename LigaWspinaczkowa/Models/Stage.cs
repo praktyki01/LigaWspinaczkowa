88972f9d27bd29a83910508af88b08113bd001c4
losttheone <kacper.kubon24@gmail.com>
2023-11-14 10:24:28 +0100

    Model

--- LigaWspinaczkowa/Models/Stage.cs
+++ LigaWspinaczkowa/Models/Stage.cs
@@ -10,16 +10,8 @@ namespace LigaWspinaczkowa.Models
         public DateTime DataFrom { get; set; }
         [Display(Name = "Do")]
         public DateTime DataTo { get; set; }
-        [Display(Name = "Wędka 1 ilosc chwytów")]
-        public int? Route1Holds { get; set; }
-        [Display(Name = "Wędka 2 ilosc chwytów")]
-        public int? Route2Holds { get; set; }
-        [Display(Name = "Prowadzenie ilosc chwytów")]
-        public int? Route3Lead { get; set; }
         [Display(Name = "Nazwa")]
         public string? Name { get; set; }
-        [Display(Name = "Opis")]
-        public string? Description { get; set; }
         public ICollection<UserStage> UserStage { get; } = new List<UserStage>();
     }
 }
